2ca7f02cdd244f62a5c2b47514e44404d8ce152c
Roy Elkabetz <elkabetzroy@gmail.com>
2021-09-27 16:35:11 +0300

    Create pyquine.py

---
+++ pyquine.py
@@ -0,0 +1,36 @@
+# This is a python quine
+# the variables
+q = chr(39)
+idx = 8
+ 
+ 
+# the string list
+a = [
+'# This is a python quine',
+'# the variables',
+'q = chr(39)',
+'idx = 8',
+' ',
+' ',
+'# the string list',
+'a = [',
+']',
+' ',
+' ',
+'# the print',
+'for i in range(idx):',
+'    print(a[i])',
+'for s in a:',
+'    print(q + s + q + ",")',
+'for i in range(idx, len(a)):',
+'    print(a[i])',
+]
+ 
+ 
+# the print
+for i in range(idx):
+    print(a[i])
+for s in a:
+    print(q + s + q + ",")
+for i in range(idx, len(a)):
+    print(a[i])
